9c3f49b758ad4a6a151fd137faac7a701a93001f
andrenfreitas1993 <admin@gilcroche.com.br>
2025-03-25 09:07:38 -0300

    confirmation flow suggestion

--- rasa-challenge/actions/action_suggested_salon.py
+++ rasa-challenge/actions/action_suggested_salon.py
@@ -16,10 +16,10 @@ class ActionTime(Action):
         domain: Dict[Text, Any],
     ) -> List[Dict[Text, Any]]:
         lista = [["Feeh Nails Decorated","Rua Nóbrega de Sousa Coutinho, 160 - Jardim Peri, São Paulo - SP, 02651-010, Brazil","Rating: 4.4"],["Espaço Carolina","Rua odassi nazzali, 63 - Jardim Santa Cruz, São Paulo - SP, 02672-010, Brazil","Rating: Não disponível"]]
-        dispatcher.utter_message("Here are some suggestions for Beauty Salon::")
+        dispatcher.utter_message("Here are some suggestions for Beauty Salon:")
         for l in lista:
             dispatcher.utter_message(f"Nome:{l[0]}")
             dispatcher.utter_message(f"Endereço:{l[1]}")
-            dispatcher.utter_message(f"Rating:{l[2]}")
+            dispatcher.utter_message(f"{l[2]}")
             dispatcher.utter_message("###################")
         return []
